c334c50de62141ce342ccd6c076b3aebfc4d714c
kaayainci-svg <kaayainci@gmail.com>
2026-03-23 23:22:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2921,9 +2921,7 @@ def redirect_short_url(code):
         return redirect(url_for('index'))
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=10000, debug=True)
     import os
-if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
     
